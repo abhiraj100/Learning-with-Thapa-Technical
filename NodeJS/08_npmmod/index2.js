8517d131ca992245aa954953cd426e7fd79cb37d
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-11-29 01:43:03 +0530

    Global npm module -> nodemon

--- NodeJS/08_npmmod/index2.js
+++ NodeJS/08_npmmod/index2.js
@@ -1,11 +1,21 @@
-const chalk = require("chalk");
-const validator = require('validator');
+// const chalk = require("chalk");
+// const validator = require('validator');
 
-// console.log(chalk.green.underline.inverse("success"));
+// // console.log(chalk.green.underline.inverse("success"));
 
+// // const result = validator.isEmail("abhiraj@abhiraj.com");
 // const result = validator.isEmail("abhiraj@abhiraj.com");
-const result = validator.isEmail("abhiraj@abhiraj.com");
-// const result = validator.isEmail("abhiraj@abhirajco.m");
-console.log(result);
+// // const result = validator.isEmail("abhiraj@abhirajco.m");
+// console.log(result);
 
-console.log(result ? chalk.green.inverse(result) : chalk.red.inverse(result));
+// console.log(result ? chalk.green.inverse(result) : chalk.red.inverse(result));
+
+(async () => {
+  const chalk = (await import("chalk")).default;
+  const validator = require("validator");
+
+  const result = validator.isEmail("abhiraj@abhiraj.com");
+  console.log(result);
+
+  console.log(result ? chalk.green.inverse(result) : chalk.red.inverse(result));
+})();
