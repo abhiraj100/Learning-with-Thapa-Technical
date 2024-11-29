e17d811fec0f71c6344beea32c7558a92c14a477
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-11-29 23:30:52 +0530

    learning about the Module Wrapper Function

---
+++ NodeJS/10_ModWrapperFun/index.js
@@ -0,0 +1,31 @@
+// ( function(exports, require, module, __filename, __dirname){    
+//     // const a = require("fs");
+//     const name = "abhiraj";
+//     console.log(name);
+//     // module.exports = {dlkjsdhasdlk}
+// } );
+
+
+// (function() {
+//     var a = "abhiraj yadav";
+//     console.log(a);
+// }) ();
+
+//     console.log(a);   // a is not defined because of iife variables & date are private in iife, we can't access it outside the function
+
+console.log(__dirname);
+console.log(__filename);
+
+
+// Private ->
+// module => in nodejs individual files are called module
+
+
+
+
+
+
+// const name = "abhiraj";
+// console.log(name);
+
+
